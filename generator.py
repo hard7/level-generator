a076541cb20b4ba75e16f7d201fe7c45644b61f8
Alexandr Nosov <anosov@ivc.nnov.ru>
2014-12-10 13:12:46 +0400

    hero & finish on border

--- generator.py
+++ generator.py
@@ -29,14 +29,32 @@ class Generator:
                 self._filled_cells.append(coord)
                 return coord
 
-    def make_field(self):
+    def rand_coord_on_border(self):
+        fix = randrange(2)
+        dim = (self._x, self._y)
+        z = [choice([0, dim[fix]-1]), randrange(dim[1 - fix])]
+        return tuple(z[::-fix*2+1])
+
+    def gen_dim(self):
         self._x, self._y = randrange(*self._x_range), randrange(*self._y_range)
+
+    def make_field(self):
+        self.gen_dim()
         field = None
         while not field or not field.is_valid():
             self._filled_cells = []
             field = Field(self._x, self._y)
-            field.add_object(self.rand_coord, Type.HERO)
-            field.add_object(self.rand_coord, Type.FINISH)
+
+            # field.add_object(self.rand_coord, Type.HERO)
+            # field.add_object(self.rand_coord, Type.FINISH)
+
+            hero_coord = self.rand_coord_on_border()
+            finish_coord = (self._x-1-hero_coord[0], self._y-1-hero_coord[1])
+
+            field.add_object(hero_coord, Type.HERO)
+            field.add_object(finish_coord, Type.FINISH)
+
+            self._filled_cells.extend([hero_coord, finish_coord])
 
             cell_count = float(self._x * self._y)
             fill = self._range_filling
@@ -78,5 +96,5 @@ if __name__ == '__main__':
     gen = Generator((3, 8), (3, 8))
     gen.set_range_filling(40, 70)
 
-    for _ in range(20):
-        field = gen.make_field()
+    # for _ in range(20):
+    #     field = gen.make_field()
--- main.py
+++ main.py
@@ -8,22 +8,25 @@ path_to_levels = 'result/levels/'
 answer_path = 'result/answer.txt'
 
 if __name__ == '__main__':
-    gen = Generator((4, 6), (4, 6))
+    gen = Generator((4, 5), (4, 5))
     gen.set_range_filling(30, 50)
     count = 0
-    answer = open(answer_path, 'w')
-    while count < 100:
+    # answer = open(answer_path, 'w')
+    while count < 10:
         f = gen.make_field()
         s = Solver(f)
         cells = f.dim[0] * f.dim[1]
         res = s.run()
-        if res and len(res)-1 >= 12 and len(res)-1 <= 16:
-            name = "GenLevel_%i" % count
-            file = open(path_to_levels + name + '.txt', 'w')
-            file.write(f.to_string(name))
-            file.close()
-            answer.write("%s <%i> %s\n" % (name, len(res)-1, res,))
+        if res and len(res)-1 >= 10 and len(res)-1 <= 16:
+            print res
+            print f.to_string()
+
+            # name = "GenLevel_%i" % count
+            # file = open(path_to_levels + name + '.txt', 'w')
+            # file.write(f.to_string(name))
+            # file.close()
+            # answer.write("%s <%i> %s\n" % (name, len(res)-1, res,))
             count += 1
-            print count
-    answer.close()
+            # print count
+    # answer.close()
 
